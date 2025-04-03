fd01a3faf9babd202917e3cbb3d551410d2f972a
Viviane Derperces <viviane.derperces@afpa-dev-pompey.fr>
2025-04-03 13:20:37 +0200

    Structure du projet

---
+++ src/main/java/fr/afpa/pompey/APIBoulangerie/ApiBoulangerieApplication.java
@@ -0,0 +1,13 @@
+package fr.afpa.pompey.APIBoulangerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBoulangerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBoulangerieApplication.class, args);
+	}
+
+}
